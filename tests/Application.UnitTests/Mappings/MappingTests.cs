fb1e83c048da61eed2bbe08e12571782d8f8cb6d
Zdravko Zdravkov <zdr0@yahoo.com>
2022-12-08 18:48:36 +0100

    Add Test projects

---
+++ tests/Application.UnitTests/Mappings/MappingTests.cs
@@ -0,0 +1,50 @@
+﻿namespace Application.UnitTests.Mappings;
+
+using System.Runtime.Serialization;
+
+using AutoMapper;
+
+using CleanArchitecture.Application.Common.Mappings;
+using CleanArchitecture.Application.Departments;
+using CleanArchitecture.Application.Employees;
+using CleanArchitecture.Domain.Entities;
+
+public class MappingTests
+{
+    private readonly IConfigurationProvider configuration;
+    private readonly IMapper mapper;
+
+    public MappingTests()
+    {
+        this.configuration = new MapperConfiguration(
+            config 
+                => config.AddProfile<MappingProfile>());
+
+        this.mapper = this.configuration.CreateMapper();
+    }
+
+    [Test]
+    public void ShouldHaveValidConfiguration()
+    {
+        this.configuration.AssertConfigurationIsValid();
+    }
+
+    [Test]
+    [TestCase(typeof(Department), typeof(DepartmentDetailsDto))]
+    [TestCase(typeof(Department), typeof(DepartmentFullDetailsDto))]
+    [TestCase(typeof(Department), typeof(DepartmentListingDto))]
+    [TestCase(typeof(Employee), typeof(EmployeeListingDto))]
+    [TestCase(typeof(Employee), typeof(EmployeeDetailsDto))]
+    [TestCase(typeof(Employee), typeof(EmployeeDto))]
+    public void ShouldSupportMappingFromSourceToDestination(Type source, Type destination)
+    {
+        var instance = this.GetInstanceOf(source);
+
+        this.mapper.Map(instance, source, destination);
+    }
+
+    private object GetInstanceOf(Type type)
+        => type.GetConstructor(Type.EmptyTypes) != null 
+            ? Activator.CreateInstance(type)! 
+            : FormatterServices.GetUninitializedObject(type);
+}
---
+++ tests/Domain.UnitTests/Entities/DepartmentTests.cs
@@ -0,0 +1,61 @@
+﻿namespace Domain.UnitTests.Entities;
+
+using CleanArchitecture.Domain.Entities;
+using FluentAssertions;
+
+public class DepartmentTests
+{
+    [Test]
+    public void PublicConstructorWorksCorrectly()
+    {
+        var department = new Department();
+
+        department.Name.Should().Be(string.Empty);
+        department.Employees.Should().NotBeNull();
+    }
+
+    [Test]
+    public void DepartmentIsCreatedCorrectly()
+    {
+        var name = "IT";
+        var date = DateTime.Now;
+
+        var department = Department.Create(name, date);
+
+        department.Id.Should().Be(0);
+        department.CreatedAt.Should().Be(date);
+        department.IsActive.Should().Be(true);
+        department.Name.Should().Be(name);
+        department.Employees.Should().NotBeNull();
+    }
+
+    [Test]
+    public void DepartmentIsRenamedCorrectly()
+    {
+        var name = "IT";
+        var date = DateTime.Now;
+
+        var department = Department.Create(name, date);
+        department.Name.Should().Be(name);
+
+        var newName = "HR";
+        department.Rename(newName);
+
+        department.Name.Should().Be(newName);
+    }
+
+    [Test]
+    public void DepartmentDeleteWorksCorrectly()
+    {
+        var name = "IT";
+        var date = DateTime.Now;
+
+        var department = Department.Create(name, date);
+
+        department.IsActive.Should().Be(true);
+
+        department.Delete();
+
+        department.IsActive.Should().Be(false);
+    }
+}

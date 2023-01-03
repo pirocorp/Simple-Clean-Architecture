84298daea3fc2ced03f923b85217884e7543a516
Zdravko Zdravkov <zdr0@yahoo.com>
2023-01-03 17:26:35 +0100

    Only README in main branch

--- src/CleanArchitecture.Infrastructure/Services/DateTimeService.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace CleanArchitecture.Infrastructure.Services;
-
-using CleanArchitecture.Application.Common.Interfaces;
-
-internal class DateTimeService : IDateTimeService
-{
-    public DateTime Now => DateTime.Now;
-}
